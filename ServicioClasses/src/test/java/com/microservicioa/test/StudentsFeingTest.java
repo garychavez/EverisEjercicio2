894bc6cb2370ad1055d49f29356899237fb36b03
garychavez <52689437+garychavez@users.noreply.github.com>
2019-07-30 15:34:40 -0500

    .

---
+++ ServicioClasses/src/test/java/com/microservicioa/test/StudentsFeingTest.java
@@ -0,0 +1,36 @@
+package com.microservicioa.test;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class StudentsFeingTest {
+
+	@Autowired
+	private MockMvc mvc;
+	
+	@Test
+	public void findAll()throws Exception{
+		this.mvc.perform(get("/Students"))
+		.andExpect(status().isOk())
+		.andExpect(content()
+		.contentType("application/json;charset=utf-8"));
+	}
+	
+	@Test
+	public void findById()throws Exception{
+		this.mvc.perform(get("/Students/1"))
+		.andExpect(status().isOk());
+	}
+}
